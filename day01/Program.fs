de593bf44f15043cb36868bc7c756207ad6ab352
Danielle Mayabb <danielle@igalia.com>
2023-12-01 15:42:18 -0800

    Add day 1 solution

---
+++ day01/Program.fs
@@ -0,0 +1,19 @@
+﻿open System.Text.RegularExpressions
+
+let GetAndSumCalibrationValues(filepath: string) =
+    let mutable total = 0
+    System.IO.File.ReadLines(filepath)
+    |> Seq.iter ( fun s1 ->
+        let filtered = Regex.Replace(s1, "[^0-9]", "")
+        let first = filtered[0]
+        let last = filtered |> Seq.last
+        let combinedChars = (first.ToString() + last.ToString())
+        let num = combinedChars |> int
+        printfn $"{num}"
+        total <- total + num
+    )
+    printfn $"{total}"
+    total
+
+
+GetAndSumCalibrationValues "./data/input.txt"
